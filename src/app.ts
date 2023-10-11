360724648a89572c8e25346b50664f17c27a0f0f
Vinicius.Pereira <vinicius.pereira@gotobiz.com.br>
2023-10-11 15:07:54 -0300

    Adicionando o plugin @fastify/jwt

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import fastify from 'fastify';
 import { AppRoutes } from './routes';
+import jwt from '@fastify/jwt';
+import 'dotenv/config';
 
 export const app = fastify({
     logger: true
+});
+
+
+app.register(jwt, {
+    secret: process.env.JWT_SECRET || '03c7c0ace395d80182db07ae2c30f034'// Só pra testar kkkkkkkk 😂
 });
 
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,30 +5,35 @@ const fakeUsers = [
         id: 1,
         name: 'John Doe',
         email: 'john.doe@example.com',
+        password: 'user1@pass2023',
         age: 30,
     },
     {
         id: 2,
         name: 'Jane Doe',
         email: 'jane.doe@example.com',
+        password: 'user2@pass2023',
         age: 25,
     },
     {
         id: 3,
         name: 'Alice',
         email: 'alice@example.com',
+        password: 'user3@pass2023',
         age: 22,
     },
     {
         id: 4,
         name: 'Bob',
         email: 'bob@example.com',
+        password: 'user4@pass2023',
         age: 35,
     },
     {
         id: 5,
         name: 'Charlie',
         email: 'charlie@example.com',
+        password: 'user5@pass2023',
         age: 40,
     },
 ];
@@ -38,8 +43,15 @@ const fakeUsers = [
 interface BodyUserRequest {
     name: string, 
     email:string, 
-    age:number
+    age:number,
+    password: string,
 }
+
+interface BodyAuthenticationRequest {
+    email: string,
+    password: string,
+}
+
 
 export async function AppRoutes(app: FastifyInstance) {
     app.get('/ping', (request, reply: FastifyReply) => {
@@ -61,13 +73,14 @@ export async function AppRoutes(app: FastifyInstance) {
         const {
             name,
             email,
-            age
+            age,
+            password
 
         } = request.body;  
 
         const newId = fakeUsers[fakeUsers.length - 1].id + 1;
 
-        fakeUsers.push({ id: newId, name, email, age });
+        fakeUsers.push({ id: newId, name, email, age, password: password });
 
 
         reply.send({
@@ -75,7 +88,37 @@ export async function AppRoutes(app: FastifyInstance) {
             message:'The user has been created'
         });
 
+    });
 
+
+    app.post('/auth', async (request:FastifyRequest<{Body:BodyAuthenticationRequest}>, reply: FastifyReply) =>{
+
+        const { email, password }: BodyAuthenticationRequest  = request.body;
+
+
+        const userExists = fakeUsers.map(a => a).filter(b => b.email === email);
+        console.log(userExists);
+
+        if(userExists.length >= 1){
+
+            if(userExists[0].password === password){
+                const token = await reply.jwtSign({
+                    email,
+                }, {expiresIn: '1d'});
+    
+    
+                return reply.send({
+                    status: 200,
+                    message: 'The user has been authenticated',
+                    token
+                });
+            }
+            
+        }
+        return reply.status(403).send({
+            status: 403,
+            message:'No Access'
+        });
 
     });
 
